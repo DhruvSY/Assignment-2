af70dc0bbe822778fb452b4e3321b8cbdedbd5da
Dhruv S Y <66639009+DhruvSY@users.noreply.github.com>
2020-06-29 18:56:46 +0530

    Add files via upload

---
+++ speakgoodbye.js
@@ -0,0 +1,8 @@
+<html><head></head><body><script type="text/javascript" id="dcoder_script">(function (Window){
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(Window);</script></body></html>
---
+++ speakhello.js
@@ -0,0 +1,8 @@
+<html><head></head><body><script type="text/javascript" id="dcoder_script">(function (Window){
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(Window);</script></body></html>
