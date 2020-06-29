a585865b6053a837dc1248d0e5209e8c17f6e7fa
Dhruv S Y <66639009+DhruvSY@users.noreply.github.com>
2020-06-29 19:00:12 +0530

    Update speakgoodbye.js

--- speakgoodbye.js
+++ speakgoodbye.js
@@ -1,8 +1,8 @@
-<html><head></head><body><script type="text/javascript" id="dcoder_script">(function (Window){
+(function (Window){
   var byeSpeaker = {};
   var speakWord = "Good Bye";
   byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   }
   window.byeSpeaker = byeSpeaker;
-})(Window);</script></body></html>
+})(Window);
