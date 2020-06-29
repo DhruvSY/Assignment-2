8e51aa6f7dcac3645e379edee182f8efad32b78f
Dhruv S Y <66639009+DhruvSY@users.noreply.github.com>
2020-06-29 19:05:07 +0530

    Update speakhello.js

--- speakhello.js
+++ speakhello.js
@@ -1,8 +1,8 @@
-<html><head></head><body><script type="text/javascript" id="dcoder_script">(function (Window){
+(function (Window){
   var helloSpeaker = {};
   var speakWord = "Hello";
   helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   }
   window.helloSpeaker = helloSpeaker;
-})(Window);</script></body></html>
+})(Window);
